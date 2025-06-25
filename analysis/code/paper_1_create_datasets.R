fe0a61705c9b0a048fd69e7093006f9fcbaa608b
meikemorren <meikemorren@hotmail.com>
2025-06-25 15:03:05 +0200

    recreated datasets

--- analysis/code/paper_1_create_datasets.R
+++ analysis/code/paper_1_create_datasets.R
@@ -99,11 +99,48 @@ for(i in seq(1, length(files))){
     annotate<-bind_rows(annotate, readxl::read_excel(paste0('./analysis/data/annotation/', files[i]),
                                                      sheet = 'Final Review') %>%
                           mutate(SDG=as.character(parse_number(gsub('_',' ',files[i])))) %>%
-                          select(`Text ID`, SDG, Text, Finn, Gib, `Jean-Baptiste`, Steve, Alignment, Consensus, Rationale)
+                          select(`Text ID`, SDG, Text, Finn, Gib, `Jean-Baptiste`, Steve, Alignment, Consensus, Rationale) 
     )
   }
 }
 
+table(annotate$Consensus)
+
+annotate<-annotate %>% 
+  mutate(Finn=as.logical(case_when(Finn=='NOT SDG 10'~ 'FALSE',
+                                   Finn=='SDG 10'~ 'TRUE',
+                                   Finn=='NOT SDG 7'~ 'FALSE',
+                                   Finn=='SDG 7'~ 'TRUE',
+                                   Finn=='SKIP'~ NA,Finn=='TO DO'~ NA, TRUE~Finn)),
+         Gib=as.logical(case_when(Gib=='NOT SDG 10'~ 'FALSE',
+                                  Gib=='SDG 10'~ 'TRUE',
+                                  Gib=='NOT SDG 7'~ 'FALSE',
+                                  Gib=='SDG 7'~ 'TRUE',
+                                  Gib=='SKIP'~ NA, Gib=='TO DO'~ NA, TRUE~Gib)),
+         `Jean-Baptiste`=as.logical(case_when(`Jean-Baptiste`=='NOT SDG 10'~ 'FALSE',
+                                              `Jean-Baptiste`=='SDG 10'~ 'TRUE',
+                                              `Jean-Baptiste`=='NOT SDG 7'~ 'FALSE',
+                                              `Jean-Baptiste`=='SDG 7'~ 'TRUE',
+                                              `Jean-Baptiste`=='SKIP'~ NA, 
+                                              `Jean-Baptiste`=='TO DO'~ NA, TRUE~`Jean-Baptiste`)),
+         Meike=as.logical(case_when(Meike=='NOT SDG 10'~ 'FALSE',
+                                    Meike=='SDG 10'~ 'TRUE',
+                                    Meike=='SKIP'~ NA, Meike=='TO DO'~ NA, TRUE~Meike)),
+         Ivan=as.logical(case_when(Ivan=='SKIP'~ NA, Ivan=='TO DO'~ NA, TRUE~Ivan)),
+         Steve=as.logical(case_when(Steve=='NOT SDG 10'~ 'FALSE',
+                                    Steve=='SDG 10'~ 'TRUE',
+                                    Steve=='NOT SDG 7'~ 'FALSE',
+                                    Steve=='SDG 7'~ 'TRUE',
+                                    Steve=='SKIP'~ NA,Steve=='TO DO'~ NA, TRUE~Steve)),
+         Consensus=as.logical(case_when(Consensus=='NOT SDG 10'~ 'FALSE',
+                                        Consensus=='SDG 10'~ 'TRUE',
+                                        Consensus=='NOT SDG 7'~ 'FALSE',
+                                        Consensus=='SDG 7'~ 'TRUE',
+                                        Consensus=='IGNORE'~ NA,TRUE~Consensus)))
+
+table(annotate$Consensus)
 
 write.csv(annotate,'./analysis/data/annotated_texts.csv')
+write.csv(annotate %>% filter(is.na(Consensus)) %>% select(`Text ID`, SDG, Text),
+          './analysis/data/unclassified_texts.csv')
 write.csv(df %>% filter(!is.na(df$sdg)),'./analysis/data/benchmark_texts.csv')
